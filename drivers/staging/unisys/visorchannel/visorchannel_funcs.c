2c71ee8ac3144c4f9ba40f72192dd6c77f7fd39c
Erik Arfvidson <erik.arfvidson@unisys.com>
2014-10-22 16:44:48 -0400

    staging: unisys: fixed braces check in visorchannel_funcs
    this patch adds braces to an else statement to remove check in
    checkpatch.pl for visorchannel_funcs.c

--- drivers/staging/unisys/visorchannel/visorchannel_funcs.c
+++ drivers/staging/unisys/visorchannel/visorchannel_funcs.c
@@ -584,8 +584,9 @@ visorchannel_debug(VISORCHANNEL *channel, int nQueues,
 		if (off == 0) {
 			phdr = &channel->chan_hdr;
 			seq_puts(seq, "(following data may be stale)\n");
-		} else
+		} else {
 			return;
+		}
 	}
 	nbytes = (ulong)(phdr->Size);
 	seq_printf(seq, "--- Begin channel @0x%-16.16Lx for 0x%lx bytes (region=0x%lx bytes) ---\n",
